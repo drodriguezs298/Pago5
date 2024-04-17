5c27e906473b5367335d773ecce2337153aef455
rcalderonc456 <123522261+rcalderonc456@users.noreply.github.com>
2024-04-17 10:30:19 -0600

    Update App.jsx
    Aqui agregue el react-router-dom. En lo personal me funciono, me redirigio a Confirmacion y espero si funcione con el Inicio.jsx

--- pago/src/App.jsx
+++ pago/src/App.jsx
@@ -1,19 +1,25 @@
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Inicio from './componentes/Inicio';
 import Compra from './componentes/Compra'
-
-
+import Confirmacion from './componentes/Confirmacion';
 
 
 function App() {
   
-  
-  return (
-    <>
-   
-   <Compra/>
-   
 
-    </>
-  )
+  return (
+<Router>
+      <Routes>
+      <Route path="/" element={<Inicio />} />
+        <Route path="/compra" element={<Compra />} />
+        {/* <Route path="/reservas" element={<Reservas />} /> */}
+       {/* <Route path="/boletos" element={<Boletos />} /> */}
+        {/* <Route path="/vuelos" element={<Vuelos />} /> */}
+        <Route path="/confirmacion" element={<Confirmacion />} />
+      </Routes>
+    </Router>
+  );
 }
   
 
